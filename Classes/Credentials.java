d5217528dcf505f7fb883489ba8f072de33115e4
smeenakshi68 <smeenakshi68@gmail.com>
2015-10-06 11:50:10 -0400

    Create Credentials.java
    SRETTING GETTER AND SETTER FOR SUPPORT, CONFIDENCE AND UUID

---
+++ Classes/Credentials.java
@@ -0,0 +1,37 @@
+package classes;
+
+public class Credentials {
+	String support;
+	String confidence;
+	String UUID;
+	
+	public void setSupport(String support)
+	{
+		this.support = support;
+	}
+	public void setConfidence(String confidence)
+	{
+		this.confidence = confidence;
+	}
+	
+	public void setUUID(String UUID)
+	{
+		this.UUID = UUID;
+	}
+	
+	public String getSupport()
+	{
+		return support;
+	}
+	public String getConfidence()
+	{
+		return confidence;
+	}
+	public String getUUID()
+	{
+		return UUID;
+	}
+	
+	
+
+}
